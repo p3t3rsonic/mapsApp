8e061977f76879497362877b361995412a27c133
Pedro Passarin <pedro_passarin@hotmail.com>
2021-10-06 22:16:45 -0300

    app finalizado

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild('map') mapRef: ElementRef;
+  map: google.maps.Map;
 
   constructor() {}
+    ionViewWillEnter(){
+      this.exibirMapa();
+    }
+
+  exibirMapa(): void{
+    const posicao = new google.maps.LatLng(-22.174222276334117, -47.39390635985415);
+    const opcoes = {
+      center: posicao,
+      zoom: 10,
+      disableDefaultUI: false
+    };
+      this.map = new google.maps.Map(this.mapRef.nativeElement, opcoes);
+  }
 
 }
